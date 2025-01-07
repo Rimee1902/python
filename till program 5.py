80a053f98c413336bdbb88581cd54c3465aa630a
Rimee1902 <rimeethakkar1902@gmail.com>
2025-01-07 15:40:20 +0530

    Add files via upload

---
+++ till program 5.py
@@ -0,0 +1,17 @@
+#subtract two numbers
+a=int(input("etnter first number :"))
+b=int(input("enter another number:"))
+c = a-b
+print(c)
+
+#multiplication of two numbers
+a=int(input("etnter first number :"))
+b=int(input("enter another number:"))
+c = a*b
+print(c)
+
+#division of two numbers
+a=int(input("etnter first number :"))
+b=int(input("enter another number:"))
+c = a/b
+print(c)
