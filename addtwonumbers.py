3cab055aeb9098d0035bba0ed5dd0240aaea7e17
Rimee1902 <rimeethakkar1902@gmail.com>
2025-01-07 15:33:24 +0530

    Add files via upload

---
+++ addtwonumbers.py
@@ -0,0 +1,15 @@
+#//addition of two numbers
+"""
+a=8
+b=18
+c=a+b
+print(a,b,c)
+print(a,"+",b,"=",c)
+a=12.75
+print(a,"+",b,"=",c)
+print(type(a))
+"""
+a=float(input("enter first number "))
+b=int(input("enter second number"))
+c=a+b
+print(c)
