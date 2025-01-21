7105cf6f46ba25c7c97e7c002746c50747be4b0f
Rimee1902 <rimeethakkar1902@gmail.com>
2025-01-21 15:11:02 +0530

    Add files via upload

---
+++ 2.4.py
@@ -0,0 +1,8 @@
+def divby10():
+    x=int(input("enter a number to check:"))
+    if(x%10==0):
+        print("the number is divisible by 10")
+    else:
+        print("the number is not divisible by 10")
+
+divby10()
