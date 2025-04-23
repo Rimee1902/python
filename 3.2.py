63939437907ec3bc0a9739bdc354a0581699b6ac
Rimee1902 <rimeethakkar1902@gmail.com>
2025-04-23 12:11:34 +0530

    Add files via upload

---
+++ 3.2.py
@@ -0,0 +1,10 @@
+
+s=str(input('enter a string: '))
+a=str(input('enter another string to be searched in string: '))
+c=0
+if a in s:
+    c+=1
+if c>0:
+    print(a,'is in string',s)
+else:
+    print(a,'not in',s)
