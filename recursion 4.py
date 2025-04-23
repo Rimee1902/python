c7ce63462265e8473f1b3dc21a9f733654ee0949
Rimee1902 <rimeethakkar1902@gmail.com>
2025-04-23 13:03:43 +0530

    Add files via upload

---
+++ recursion 4.py
@@ -0,0 +1,11 @@
+def revlist(l):
+     if len(l)==0:
+          return []
+     else:
+          return [l[-1]] + revlist(l[:-1])    
+print(revlist([1,2,3]))   def revlist(l):
+     if len(l)==0:
+          return []
+     else:
+          return [l[-1]] + revlist(l[:-1])    
+print(revlist([1,2,3]))   
