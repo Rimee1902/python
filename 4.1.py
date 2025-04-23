ed042ae50d0f0fa57dcb9e7bac16b6f3ee0b1ded
Rimee1902 <rimeethakkar1902@gmail.com>
2025-04-23 12:25:29 +0530

    Add files via upload

---
+++ 4.1.py
@@ -0,0 +1,19 @@
+
+for i in range(24):
+    if i==0:
+        print(i,"midnight")
+    if i<12 and i!=0:
+        print(i,"am")
+    if i==12:
+        print(i,"noon")
+    if i>12:
+        print(i,"pm")
+for i in range(24):
+    if i==0:
+        print(i,"midnight")
+    if i<12 and i!=0:
+        print(i,"am")
+    if i==12:
+        print(i,"noon")
+    if i>12:
+        print(i,"pm")
